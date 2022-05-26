757a07f79391df373ae68ae4a243f2ba6e10d764
Julia Martins <mjuh.ms@gmail.com>
2022-05-26 11:53:44 -0300

    Adding `mongoose`

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+
+const port = process.env.PORT || 3000
+const mongoURI = process.env.MONGO_URI
+const Schema = mongoose.Schema;
 
 app.use(cors())
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+
+const userSchema = new Schema({
+  username: { type: String, required: true }
+})
+
+let User = mongoose.model("User", userSchema)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -17,13 +30,24 @@ app.get('/', (req, res) => {
 // POST /api/users
 app.post('/api/users', (req, res) => {
   const username = req.body.username
-  res.json({ username })
+
+  new User({ username })
+    .save()
+    .then(user => {
+      res.json({
+        username,
+        _id: user._id
+      })
+    })
+    .catch(err => {
+      res.json(err)
+    })
 })
 
 //POST /api/users/:_id/exercises
 
 //GET /api/users/:_id/logs?[from][&to][&limit]
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(port, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
