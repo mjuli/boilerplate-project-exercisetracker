8efbfe8dc01d0956f36100b0068950cdfbb92daa
Julia Martins <mjuh.ms@gmail.com>
2022-05-26 11:33:51 -0300

    Adding `body-parser` Adding `/api/users` post route

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// POST /api/users
+app.post('/api/users', (req, res) => {
+  const username = req.body.username
+  res.json({ username })
+})
 
+//POST /api/users/:_id/exercises
 
-
+//GET /api/users/:_id/logs?[from][&to][&limit]
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
