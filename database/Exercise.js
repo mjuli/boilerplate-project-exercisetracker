491b0b6bfc5004c8dfe2cbbcbfdc962a8bd6c048
Julia Martins <mjuh.ms@gmail.com>
2022-05-26 15:18:55 -0300

    Changing `_id` to `userId`

--- database/Exercise.js
+++ database/Exercise.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-  _id: { type: mongoose.ObjectId, required: true },
+  userId: { type: String, required: true },
   description: { type: String },
   duration: { type: Number },
   date: { type: Date}
